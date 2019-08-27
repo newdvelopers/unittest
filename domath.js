cb1ab4297682acbbdc16fa8fba8ce7e779cb6b22
newdvelopers <newdvelopers@gmail.com>
2019-08-27 16:10:47 +0530

    adding 3 numbers in do math

--- domath.js
+++ domath.js
@@ -2,8 +2,12 @@ function multiply(a, b) {
   return a * b;
 }
 
+function addthreenumbers(a, b, c) {
+  return a + b + c;
+}
 
 
 module.exports = {
-  multiply
+  multiply,
+  addthreenumbers
 }
--- domath.test.js
+++ domath.test.js
@@ -1,13 +1,14 @@
-const { multiply } = require('./domath');
+const { multiply,addthreenumbers } = require('./domath');
 
 /**
  * Multiply to be defined
  */
  
  
- test('Multiply function exists', () => {
+test('Multiply function exists', () => {
   expect(multiply).toBeDefined();
 });
+
 
 
 test('multiply 5 * 5 to equal 25', mulTest);
@@ -17,5 +18,13 @@ function mulTest() {
 }
 
 
+test('Addthreenumbers function exists', () => {
+  expect(addthreenumbers).toBeDefined();
+});
 
 
+test('add 3 + 3 + 3 to equal 9', addThree);
+
+function addThree() {
+  expect(addthreenumbers(3, 3, 3)).toBe(9);
+}
